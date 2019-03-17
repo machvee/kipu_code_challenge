6d66a88c13e86f5404b5a25825033c6330c60c30
Dave McVicar <machvee@gmail.com>
2019-03-17 14:43:51 -0400

    cleaner template_field helper removes boiler plate grid cols

--- app/helpers/admissions_helper.rb
+++ app/helpers/admissions_helper.rb
@@ -1,8 +1,12 @@
 module AdmissionsHelper
-  def template_field(field_name)
+  def template_field(field_name, header: nil, col: 'col')
     t = TemplateField[field_name]
     if t.present?
-      content_tag(:div, t.substitute_for(@patient), class: "text-primary")
+      hdr = header.nil? ? field_name.titleize : header
+      content_tag(:div, class: [col, "border"]) do
+        concat content_tag(:div, hdr, class: "h6")
+        concat content_tag(:div, t.substitute_for(@patient), class: "text-primary")
+      end
     else
       content_tag(:div, "unknown template field #{field_name}", class: "text-danger")
     end
